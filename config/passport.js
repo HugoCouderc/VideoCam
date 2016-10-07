0c882cbbb6a2d2294f7dc7d702c54b6f80d4dcd5
Maxime Soubiran <Maxime Soubiran>
2016-10-07 09:42:32 +0200

    routes/policies and isAuth

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
-var EXPIRES_IN = '3d';
+var EXPIRES_IN = '2d';
 var SECRET = process.env.tokenSecret || "4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTbNg7jO41EAtl20J5F7Trtwe7OM";
 var ALGORITHM = "HS256";
 
