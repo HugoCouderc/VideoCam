734d4826efd5788661120d8eaed4bde7bd8c8af3
Boudat David <david.boudat@ynov.com>
2016-10-15 03:46:53 +0200

    Changing 2 fileName, adding a route and a CSS
    - Change file name of registerUser & rightManager to registeruser &
    rightmanager
    - add a route to registeruser (/registeruser)
    - add videocam.css

--- config/routes.js
+++ config/routes.js
@@ -35,15 +35,17 @@ module.exports.routes = {
   '/': {
     view: 'homepage'
   },
-  '/rightManager': {
-    view: 'rightManager'
+  '/rightmanager': {
+    view: 'rightmanager'
   },
   '/addcamera': {
     view:'addcamera'
   },
-  '/personalcameras':
-  {
+  '/personalcameras': {
       view:'personalcameras'
+  },
+  '/registeruser': {
+	  view : 'registeruser'
   },
 
   'post /auth/signin'  : 'AuthController.signin',
